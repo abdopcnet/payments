980198afe6b7626476ea310678ca6a6a05d5ea60
abdopcnet <abdopcnet@gmail.com>
2025-12-14 08:46:06 +0000

    🔄 code_payment_gateways.js

--- payments/payment_gateways/doctype/code_payment_gateways/code_payment_gateways.js
+++ payments/payment_gateways/doctype/code_payment_gateways/code_payment_gateways.js
@@ -1,8 +1,13 @@
 // Copyright (c) 2025, Frappe Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Code Payment Gateways", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Code Payment Gateways", {
+  onload: function (frm) {
+    frm.set_query("student", function () {
+      return {
+        query:
+          "payments.payments.payment_gateways.doctype.code_payment_gateways.code_payment_gateways.get_students",
+      };
+    });
+  },
+});
