4fa76f75d005a72de2105b6464b50eab7726a3cb
abdopcnet <abdopcnet@gmail.com>
2025-12-18 17:04:30 +0000

    🔄 code_payment_gateways.js

--- payments/payment_gateways/doctype/code_payment_gateways/code_payment_gateways.js
+++ payments/payment_gateways/doctype/code_payment_gateways/code_payment_gateways.js
@@ -10,4 +10,28 @@ frappe.ui.form.on("Code Payment Gateways", {
       };
     });
   },
+  
+  code_amount: function(frm) {
+    if (frm.doc.code_amount && !frm.doc.free_code) {
+      if (frm.is_new()) {
+        frm.set_value("code_remaining_amount", frm.doc.code_amount);
+        frm.set_value("code_used_amount", 0);
+      } else {
+        const remaining = parseFloat(frm.doc.code_remaining_amount || 0);
+        const used = parseFloat(frm.doc.code_used_amount || 0);
+        if (remaining === 0 && used === 0) {
+          frm.set_value("code_remaining_amount", frm.doc.code_amount);
+          frm.set_value("code_used_amount", 0);
+        }
+      }
+    }
+  },
+  
+  free_code: function(frm) {
+    if (frm.doc.free_code) {
+      frm.set_value("code_amount", 0);
+      frm.set_value("code_remaining_amount", 0);
+      frm.set_value("code_used_amount", 0);
+    }
+  }
 });
